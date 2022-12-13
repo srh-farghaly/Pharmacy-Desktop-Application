554a7cb26108856536f13d95b59b3a6aefa5a123
Seifeldin-Ahmed <120275931+Seifeldin-Ahmed@users.noreply.github.com>
2022-12-13 17:51:40 +0200

    Add files via upload

---
+++ src/Modeling/DBOperation.java
@@ -0,0 +1,45 @@
+package Modeling;
+import java.sql.*;
+import javax.swing.*;
+
+/*
+DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+   LocalDateTime now = LocalDateTime.now();
+   System.out.println(dtf.format(now));
+*/
+public class DBOperation {
+
+    static Connection con;
+    static Statement st;
+
+    public static void setDataOrDelete(String Query, String msg) {
+
+        Connector c1 = new Connector();
+        try {
+
+            con = c1.connect();
+
+            st = con.createStatement();
+            st.executeUpdate(Query); // same as st.execute(Query);
+
+            if (!msg.equals("")) {
+                JOptionPane.showMessageDialog(null, msg);
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e, "Message", JOptionPane.ERROR_MESSAGE);
+        } 
+    }
+
+    public static ResultSet getData(String query) {
+        Connector c2 = new Connector();
+        try {
+            con = c2.connect();
+            st = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            ResultSet rs = st.executeQuery(query);
+            return rs;
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e, "Message", JOptionPane.ERROR_MESSAGE);
+            return null;
+        } 
+    }
+}
---
+++ src/Modeling/Pharmacist_Model.java
@@ -0,0 +1,111 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modeling;
+
+/**
+ *
+ * @author Dell
+ */
+public class Pharmacist_Model {
+    private String Firstname,Lastname,gender,phonenumber,email,password,Hired_date;
+    private int  id,salary,age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getHired_date() {
+        return Hired_date;
+    }
+
+    public void setHired_date(String Hired_date) {
+        this.Hired_date = Hired_date;
+    }
+
+
+    public Pharmacist_Model(int id,String Hired_date,String Firstname, String Lastname, String gender, String phonenumber, String email, String password, int salary, int age) {
+        this.Firstname = Firstname;
+        this.Lastname = Lastname;
+        this.gender = gender;
+        this.phonenumber = phonenumber;
+        this.email = email;
+        this.password = password;
+        this.salary = salary;
+        this.age = age;
+        this.id=id;
+        this.Hired_date=Hired_date;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public String getFirstname() {
+        return Firstname;
+    }
+
+    public void setFirstname(String Firstname) {
+        this.Firstname = Firstname;
+    }
+
+    public String getLastname() {
+        return Lastname;
+    }
+
+    public void setLastname(String Lastname) {
+        this.Lastname = Lastname;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    
+    public String getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(String phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+}
